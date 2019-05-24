2e86ab4d68096652dd2de432a7f6ec74cf62a3cf
ga03itbwip5 <40009972+ga03itbwip5@users.noreply.github.com>
2019-05-24 09:13:54 -0500

    new

--- database/include/Player.h
+++ database/include/Player.h
@@ -5,8 +5,8 @@
 class Player
 {
     public:
-        Player(string nickname, string name, string lastname, string country,int age, int gender ){};
-        ~Player(){};
+        Player(string nickname, string name, string lastname, string country,int age, int gender );
+        ~Player();
         /// get y set para todo 12 prototipos.
         /// print datos del jugador
         string getNickname();
--- database/src/Player.cpp
+++ database/src/Player.cpp
@@ -1,9 +1,49 @@
 #include "Player.h"
 
-Player::Player(string nickname, string name, string lastname,
-                string country,int age, int gender ){
+Player::Player(string nickname, string name, string lastname,string country,int age, int gender ){
+    this->nickname = nickname;
+    this->name = name;
+    this->lastname = lastname;
+    this->country = country;
+    this->age=age;
+    this->gender=gender;
+}
+string getNickname(){
+    return this->nickname;
+}
+string getName();{
+    return this->name;
+}
+string getLastname(){
+    return this->lastname;
+}
+string getCountry(){
+    return this->country;
+}
+int getAge(){
+    return this->age;
+}
+int getGender(){
+    return this->gender;
+}
 
-
+void setNickname(string nickname){
+    this->nickname = nickname;
+}
+void setName(string name){
+    this->name = name;
+}
+void setLastname(string lastname){
+    this->lastname = lastname;
+}
+void setCountry(string country){
+    this->country = country;
+}
+void setAge(int age){
+    this->age = age;
+}
+void setGender (int gender){
+    this->gender = gender;
 }
 
 Player::~Player()
